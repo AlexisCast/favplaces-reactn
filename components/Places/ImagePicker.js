f7a50082e14b0d1498b08cbede9f0cfa4a0fbb77
Alexis Castellanos <alexiscast@Alexiss-MacBook-Pro-2.local>
2023-10-07 13:04:51 -0600

    show an image preview

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,17 +1,24 @@
-import { Button, View } from "react-native";
+import { Alert, Button, Image, StyleSheet, Text, View } from "react-native";
 import {
 	launchCameraAsync,
 	useCameraPermissions,
 	PermissionStatus,
 } from "expo-image-picker";
+import { useState } from "react";
+
+import { Colors } from "../../constants/colors";
 
 const ImagePicker = () => {
+	const [pickedImage, setPickedImage] = useState();
+
 	const [cameraPermissionInformation, requestPermission] =
 		useCameraPermissions();
 
 	const verifyPermissions = async () => {
 		if (
-			cameraPermissionInformation.status === PermissionStatus.UNDETERMINED
+			cameraPermissionInformation.status ===
+				PermissionStatus.UNDETERMINED ||
+			cameraPermissionInformation.status === PermissionStatus.DENIED
 		) {
 			const permissionResponse = await requestPermission();
 
@@ -41,15 +48,42 @@ const ImagePicker = () => {
 			aspect: [16, 9],
 			quality: 0.5,
 		});
-		console.log(image);
+
+		// console.log(image.assets[0]);
+		setPickedImage(image.assets[0].uri);
 	};
+
+	let imagePreview = <Text>No image taken yet.</Text>;
+
+	if (pickedImage) {
+		imagePreview = (
+			<Image style={styles.image} source={{ uri: pickedImage }} />
+		);
+	}
 
 	return (
 		<View>
-			<View></View>
+			<View style={styles.imagePreview}>{imagePreview}</View>
+
 			<Button title="Take Image" onPress={takeImagehandler} />
 		</View>
 	);
 };
 
 export default ImagePicker;
+
+const styles = StyleSheet.create({
+	imagePreview: {
+		width: "100%",
+		height: 200,
+		marginVertical: 8,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: Colors.primary100,
+		borderRadius: 4,
+	},
+	image: {
+		width: "100%",
+		height: "100%",
+	},
+});
