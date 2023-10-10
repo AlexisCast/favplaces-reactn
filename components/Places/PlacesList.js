3f0399ff7c1f9d0ac6df7d4b33a8110c7b465dd6
Alexis Castellanos <castellanos.alexis@hotmail.com>
2023-10-10 16:46:51 -0600

    add PlaceDetails screen

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,7 +1,16 @@
+import { useNavigation } from "@react-navigation/native";
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import PlaceItem from "./PlaceItem";
 
 const PlacesList = ({ places }) => {
+	const navigation = useNavigation();
+
+	const selectPlaceHandler = (id) => {
+		navigation.navigate("PlaceDetails", {
+			placeId: id,
+		});
+	};
+
 	if (!places || places.length === 0) {
 		return (
 			<View style={styles.fallbackContainer}>
@@ -17,7 +26,9 @@ const PlacesList = ({ places }) => {
 			style={styles.list}
 			data={places}
 			keyExtractor={(item) => item.id}
-			renderItem={({ item }) => <PlaceItem place={item} />}
+			renderItem={({ item }) => (
+				<PlaceItem place={item} onSelect={selectPlaceHandler} />
+			)}
 		/>
 	);
 };
