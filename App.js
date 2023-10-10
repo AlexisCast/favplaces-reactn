75173fe7b7b848a69f0dfbbdd6105ff577197b89
Alexis Castellanos <castellanos.alexis@hotmail.com>
2023-10-10 15:41:26 -0600

    initialization

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { useCallback, useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -7,14 +8,41 @@ import AddPlace from "./screens/AddPlace";
 import IconButton from "./components/UI/IconButton";
 import { Colors } from "./constants/colors";
 import Map from "./screens/Map";
+import { init } from "./util/database";
+import * as SplashScreen from "expo-splash-screen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+	const [dbInitialized, setDbInitialized] = useState(false);
+
+	// code from Expo using SplashScreen:
+	useEffect(() => {
+		const prepare = async () => {
+			try {
+				await SplashScreen.preventAutoHideAsync();
+				init();
+			} catch (e) {
+				console.warn(e);
+			} finally {
+				setDbInitialized(true);
+			}
+		};
+		prepare();
+	}, []);
+
+	const onLayoutRootView = useCallback(async () => {
+		if (dbInitialized) {
+			await SplashScreen.hideAsync();
+		}
+	}, [dbInitialized]);
+
+	if (!dbInitialized) return null;
+
 	return (
 		<>
 			<StatusBar style="dark" />
-			<NavigationContainer>
+			<NavigationContainer onReady={onLayoutRootView}>
 				<Stack.Navigator
 					screenOptions={{
 						headerStyle: { backgroundColor: Colors.primary500 },
