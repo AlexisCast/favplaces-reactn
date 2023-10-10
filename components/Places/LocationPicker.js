c2909ca2dd655e973e9c1994b40141ae103b93d8
Alexis Castellanos <alexiscast@Alexiss-MacBook-Pro-2.local>
2023-10-10 14:48:39 -0600

    convert pickde location to human readable address

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -13,7 +13,7 @@ import {
 
 import { Colors } from "../../constants/colors";
 import OutlinedButton from "../UI/OutlinedButton";
-import { getMapPreview } from "../../util/location";
+import { getAddress, getMapPreview } from "../../util/location";
 
 const LocationPicker = ({ onPickLocation }) => {
 	const [pickedLocation, setPickedLocation] = useState();
@@ -36,7 +36,17 @@ const LocationPicker = ({ onPickLocation }) => {
 	}, [route, isFocused]);
 
 	useEffect(() => {
-		onPickLocation(pickedLocation);
+		async function handleLocation() {
+			if (pickedLocation) {
+				const address = await getAddress(
+					pickedLocation.lat,
+					pickedLocation.lng
+				);
+				onPickLocation({ ...pickedLocation, address: address });
+			}
+		}
+
+		handleLocation();
 	}, [pickedLocation, onPickLocation]);
 
 	const verifyPermissions = async () => {
